d231f50038237f62da6c709f4e6c1e91ad3084e4
HerGousis <her.gousis@gmail.com>
2025-02-08 15:48:52 +0200

    edit

--- src/FCFS.c
+++ src/FCFS.c
@@ -5,9 +5,7 @@
 
 
 
-void fcfs(QueueInt_h job_q, QueueInt_h ready_q, QueueInt_h waiting_q, PCB_vec **processes){
-
-
+void fcfs(QueueInt_h job_q, QueueInt_h ready_q, QueueInt_h waiting_q, PCB_vec **processes) {
     int timeArrival_pop[4];
     int pid_sorted[4];
     int burst[4];
@@ -15,70 +13,71 @@ void fcfs(QueueInt_h job_q, QueueInt_h ready_q, QueueInt_h waiting_q, PCB_vec **
     int priority[4];
     int temp;
 
-    for ( int i = 0; i < 4; i++)
-    {
-       timeArrival_pop[i] = pcbVec_get(processes, i).arrival;
-       pid_sorted[i] =pcbVec_get(processes, i).pid;
-       burst[i] =pcbVec_get(processes, i).burst;
-       remaining[i] =pcbVec_get(processes, i).remaining;
-       priority[i] =pcbVec_get(processes, i).priority;
-
+    // Γέμισμα των πινάκων με τα δεδομένα των διαδικασιών
+    for (int i = 0; i < 4; i++) {
+        timeArrival_pop[i] = pcbVec_get(processes, i).arrival;
+        pid_sorted[i] = pcbVec_get(processes, i).pid;
+        burst[i] = pcbVec_get(processes, i).burst;
+        remaining[i] = pcbVec_get(processes, i).remaining;
+        priority[i] = pcbVec_get(processes, i).priority;
     }
-    
-	
-	 // Bubble Sort  με βαση τους χρονους arrival  απο το μικροτερο προς το μελαυτερο 
-     // timeArrival_pop[0]=μικρο 
-     // timeArrival_pop[3]= μεγαλυτερο 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4 - i -1 ; j++) {
-                if (timeArrival_pop[j] > timeArrival_pop[j + 1]) {
-                    // Ανταλλαγή θέσεων (swap)
-                    temp = timeArrival_pop[j];
-                    timeArrival_pop[j] = timeArrival_pop[j + 1];
-                    timeArrival_pop[j + 1] = temp;
 
+    // Bubble Sort για ταξινόμηση με βάση τους χρόνους άφιξης (arrival)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4 - i - 1; j++) {
+            if (timeArrival_pop[j] > timeArrival_pop[j + 1]) {
+                // Ανταλλαγή θέσεων με χρήση της μεταβλητής temp
+                temp = timeArrival_pop[j];
+                timeArrival_pop[j] = timeArrival_pop[j + 1];
+                timeArrival_pop[j + 1] = temp;
 
-                    temp = pid_sorted[j];
-                    pid_sorted[j] = pid_sorted[j + 1];
-                    pid_sorted[j + 1] = temp;
+                temp = pid_sorted[j];
+                pid_sorted[j] = pid_sorted[j + 1];
+                pid_sorted[j + 1] = temp;
 
-                    temp = burst[j];
-                    burst[j] = burst[j + 1];
-                    burst[j + 1] = temp;
+                temp = burst[j];
+                burst[j] = burst[j + 1];
+                burst[j + 1] = temp;
 
+                temp = remaining[j];
+                remaining[j] = remaining[j + 1];
+                remaining[j + 1] = temp;
 
-                    temp = remaining[j];
-                    remaining[j] = remaining[j + 1];
-                    remaining[j + 1] = temp;
-
-                    temp = priority[j];
-                    priority[j] = priority[j + 1];
-                    priority[j + 1] = temp;
-
-                }
+                temp = priority[j];
+                priority[j] = priority[j + 1];
+                priority[j + 1] = temp;
             }
         }
+    }
+
+    // Εκτύπωση και προσθήκη των διεργασιών στην ουρά με βάση τον χρόνο burst
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < burst[i]; j++) {
+            q_push(&job_q, pid_sorted[i]);
+            q_push(&ready_q, &job_q);
+            printf("Process %d pushed to ready queue (burst time %d)\n", pid_sorted[i], burst[i]);
+        }
+    }
+
+   
+}
 
 
-     for (int i=0; i<= burst[0]; i++){
-        q_push(&job_q, pid_sorted[0]);
-        q_push(&ready_q, &job_q);
-     }
 
-     for (int i=0; i<= burst[1]; i++){
-        q_push(&job_q, pid_sorted[1]);
-        q_push(&ready_q, &job_q);
-     }
 
-     for (int i=0; i<= burst[2]; i++){
-        q_push(&job_q, pid_sorted[2]);
-        q_push(&ready_q, &job_q);
-     }
+int main()
+{
+   PCB_vec *processes = pcbVec_new();
+   read_JSON_ProcessF("processes/01_processes.json", &processes);
 
-     for (int i=0; i<= burst[3]; i++){
-        q_push(&job_q, pid_sorted[3]);
-        q_push(&ready_q, &job_q);
-     }
+	QueueInt_h job_q, ready_q, waiting_q;
+	int running;
+	q_new(&job_q);
+	q_new(&ready_q);
+	q_new(&waiting_q);
 
+   fcfs(job_q, waiting_q, ready_q , &processes ) ;
+
+   return 0;
 
 }
